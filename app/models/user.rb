dd0315a3a3900de297d872f3c66f2b3758475b03
David Towster <djt4xe@virginia.edu>
2018-03-26 16:48:05 -0700

    Created a method in user.rb to handle OmniauthCallBacksController#facebook action, which redirects to  a 'after_sign_in_path_for' action in ApplicationController, which was written in this commit as well

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+
+  #Sets user based on data received from Omniauth Facebook Login process
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|   #Finds or creates a user based on the auth.provider (Facebook) and the uid Facebook provides
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]   #This generates a random password for Facebook Login users, assuming they don't want to subsequently set a password
+    end      
+  end
 end
