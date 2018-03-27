c773839f4642407b80daf96e26dcd70aac4cf676
David Towster <djt4xe@virginia.edu>
2018-03-27 01:10:11 -0700

    Created Store model migration and migrated db, created a Store.rb model class file, and established association between Store and User objects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180327045324) do
+ActiveRecord::Schema.define(version: 20180327080515) do
+
+  create_table "stores", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
