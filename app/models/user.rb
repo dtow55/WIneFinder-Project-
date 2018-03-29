efc1dddadab418d11d8ff9020201c806b996128e
David Towster <djt4xe@virginia.edu>
2018-03-29 03:41:36 -0500

    Established User has_many Wines relationship.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :stores
+  has_many :wines, through: :stores
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
