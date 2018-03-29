724bfc13b76faf593b149f31ce7cb946a0d4f4f8
David Towster <djt4xe@virginia.edu>
2018-03-29 13:47:14 -0500

    Made adjustments to a helper method that tests whether a signed-in user is looking at their own user show page. Made changes to views to ensure any edit buttons are only shown if user is on their on page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
-  def verify_user(params)
-    params[:id].to_i == current_user.id
+  def own_user_page?(user_id)
+    user_id == current_user.id if user_signed_in?
   end
 
 end
