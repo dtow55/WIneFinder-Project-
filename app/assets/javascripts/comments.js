780a46ad1c8fe0d27ebc0fe6ff79a50266df20bc
David Towster <djt4xe@virginia.edu>
2018-04-16 16:06:58 -0700

    Started building out loadComments() function in comments.js. Started building out load-comments-template Handlebars template in wines/show file. Added jquery library to application layout.

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,3 +1,8 @@
-function comments() {
-  console.log('works');
+// display comments index on wines/:id/comments nested index 
+function loadComments(element) {
+  $.get(`${element.dataset.wine_id}/comments`, function(response) {
+    const comments = JSON.parse(response);
+    console.log(comments);
+    $("#show-comments").text('yes');
+  });
 }
