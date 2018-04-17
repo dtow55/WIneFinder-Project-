320479c395ac8efee087934f687cf65ad876cff5
David Towster <djt4xe@virginia.edu>
2018-04-17 00:23:11 -0700

    Added :print method attribute to WineSerializer. Built showWines() in wines.js (JS wines index view) Edited 'Browse Wines' button on home page to invoke showWines() on click instead of routing to wines#index. I made a high-level decision to use the home page links to link to lead to a 'parallel' JS implementation of certain pages, while the navbar will link to the original html implementation of the app. I did this to be able to compare the different implementations in case there are any differences.

--- app/assets/javascripts/wines.js
+++ app/assets/javascripts/wines.js
@@ -1,3 +1,12 @@
+function showWines() {
+  $.get("wines.json", function(response) {
+    wines = JSON.parse(response);
+    let template = Handlebars.compile($("#show-wines-template").html());
+    let result = template(wines);
+    $("#main").html(result);
+  });
+}
+
 function showWine(element) {
 
 }
--- app/serializers/wine_serializer.rb
+++ app/serializers/wine_serializer.rb
@@ -1,5 +1,5 @@
 class WineSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :price, :color, :grape
+  attributes :id, :name, :description, :price, :color, :grape, :print
   has_many :stores
   has_many :comments
 end
