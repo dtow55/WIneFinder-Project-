2101404d293c334f50c94ea6ce5ab0c1242982c1
David Towster <djt4xe@virginia.edu>
2018-04-17 00:30:01 -0700

    Made showStores template name change in both files. Also started building out show-wines-template in application.html.erb.

--- app/assets/javascripts/stores.js
+++ app/assets/javascripts/stores.js
@@ -2,7 +2,7 @@ function showStores() {
   $.get("/stores.json", function (response) {
     const stores = JSON.parse(response);
     
-    let template = Handlebars.compile($("#store-index-template").html());
+    let template = Handlebars.compile($("#show-stores-template").html());
     let result = template(stores);
     $("#main").html(result);
   });
