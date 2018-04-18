809f982d8cf8077c31eae7e1f4569b36caeefec6
David Towster <djt4xe@virginia.edu>
2018-04-18 10:22:41 -0700

    Created a JS Model Object Prototype, createComment(), gave it a print() function and used that function to print out newly created comments.

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,3 +1,40 @@
+function createComment() {
+  return class {
+    constructor(content) {
+      this.content = content;
+    }
+    print() {
+      console.log(this.content);
+      return this.content;
+    }
+  }
+}
+const Comment = createComment();
+
+function newCommentForm(element) {
+  wineId = element.dataset.wineid;
+  
+  let template = Handlebars.compile($("#comment-form-template").html());
+  let result = template(wineId);
+  $("#post-comment").html(result);
+  
+  
+  $('form').submit(postComment);
+}
+
+function postComment(event) {
+  //prevent form from submitting the default way
+  event.preventDefault();
+  
+  let values = $(this).serialize();
+  
+  let posting = $.post(`/comments`, values, function(response) {
+    const comment = new Comment(JSON.parse(response).content);
+
+    $("#show-comments").append(`${comment.print()}<br>`)
+  });
+}
+
 function loadComments(element) {
   $.get(`${element.dataset.wine_id}/comments`, function(response) {
     const comments = JSON.parse(response);
@@ -5,26 +42,5 @@ function loadComments(element) {
     let template = Handlebars.compile($("#load-comments-template").html());
     let result = template(comments);
     $("#show-comments").html(result);
-  });
-}
-
-function newCommentForm(element) {
-  wineId = element.dataset.wineid;
-
-  let template = Handlebars.compile($("#comment-form-template").html());
-  let result = template(wineId);
-  $("#post-comment").html(result);
-  
-
-  $('form').submit(function(event) {
-    //prevent form from submitting the default way
-    event.preventDefault();
-    
-    let values = $(this).serialize();
-
-    let posting = $.post(`/comments`, values, function(response) {
-      comment = JSON.parse(response);
-      $("#show-comments").append(`${comment.content}<br>`)
-    });
   });
 }
